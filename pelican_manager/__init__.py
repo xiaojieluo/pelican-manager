4042c151f1f1bfc1c88efe02089f0a84235e0631
xiaojieluo <xiaojieluoff@gmail.com>
2018-02-03 13:56:30 +0800

    添加 config monkey patch

--- pelican_manager/__init__.py
+++ pelican_manager/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from pelican_manager.config import Config
 
-def make_app():
+def make_app(conf_path = None):
+    '''
+    Args:
+        从命令行指定 config 的文件位置
+    '''
+    if conf_path:
+        Config.monkey_patch(conf_path)
+
     config = Config()
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'Security'
-    # app.debug = config['server']['debug']
-    app.debug = False
+    app.debug = config.server_debug
 
     from pelican_manager.views import admin_bp, article_bp
     app.register_blueprint(admin_bp)
