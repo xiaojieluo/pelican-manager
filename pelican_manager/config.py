e758386215e48c7476f3df8e4ebf50a82113f377
xiaojieluo <xiaojieluoff@gmail.com>
2018-02-03 13:54:48 +0800

    重构 config

--- pelican_manager/config.py
+++ pelican_manager/config.py
@@ -1,99 +1,98 @@
 import sys, os
-import imp
-import toml
 from .utils import import_module
+import re
 
-class PelicanConfig(object):
-    def __init__(self, path):
-        self.path = path
-        self.parse_config()
+class Config(object):
+    # monkey patch
+    # 保存从命令行传入的 config file path
+    config_file = None
 
-    def parse_config(self):
-        '''parse pelican config
+    def __init__(self, path = None):
+        if path is None:
+            path = os.path.join(os.getcwd(), 'pelicanconf.py')
+        if self.config_file:
+            path = self.config_file
+
+        self._path = path
+        self._pelicanconf = self.make_config(path, 'pelicanconf')
+        here = os.path.abspath(os.path.dirname(__file__))
+        default_path = os.path.join(here, 'config/defaultconf.py')
+        self._default = self.make_config(default_path, 'defaultconf')
+
+    @classmethod
+    def monkey_patch(cls, path):
+        cls.config_file = path
+
+    def set(self, key, value):
+        self.__dict__[key] = value
+
+    def get(self, key, default = None):
+        '''从 pelicanconf.py 中取出配置
+        Args:
+            key: 要取出的变量名
+            default: 配置不存在时的默认值
         '''
-        self.pelicanconf = import_module('pelicanconf', self.path)
-
-    def __getattr__(self, key):
-        return self.__getitem__(key)
-
-    def __getitem__(self, key):
-        if self.__dict__.get(key, None) is None:
-            try:
-                return getattr(self.pelicanconf, key)
-            except Exception as e:
-                print(e)
-                return None
-
-class Singleton(object):
-    _instance = None
-    def __new__(cls, *args, **kw):
-        if not cls._instance:
-            cls._instance = super().__new__(cls, *args, **kw)
-        return cls._instance
-
-class Config(dict, Singleton):
-    ''' pelican_manager config'''
-    # _instance = None
-    # def __new__(cls, *args, **kw):
-    #     if not cls._instance:
-    #         cls._instance = super(Config, cls).__new__(cls, *args, **kw)
-    #     return cls._instance
-
-    def __init__(self):
-        dict.__init__(self, self.make_config())
-
-    def filter_data(self, data):
-        server = ['port', 'debug']
-        blog = ['path']
-
-        conf = {
-            'server': {},
-            'blog': {},
-        }
-        for key, value in data.items():
-            if key in server:
-                if key == 'debug':
-                    value = bool(value)
-                conf['server'][key] = value
-            elif key in blog:
-                conf['blog'][key] = value
-
-        filter_conf = {k:v for k, v in conf.items() if v}
-        return filter_conf
-
-    def update(self, *args, **kw):
-        data ={}
-        if args:
-            data.update(args[0])
-        if kw:
-            data.update(kw)
-
-        new_config = self.filter_data(data)
-        super().update(new_config)
-
-    def save(self):
-        ''' 将 config 写入当前工作目录的 pelican_manager.toml 文件中'''
-        toml_string = toml.dumps(self)
-        path = os.path.join(os.getcwd(), 'pelican_manager.toml')
-        with open(path, 'w') as fp:
-            fp.write(toml_string)
-
-    def make_config(self):
-        '''make config'''
-        path = os.path.join(os.getcwd(), 'pelican_manager.toml')
-        default = self.parse_toml()
-        if os.path.exists(path):
-            custom = self.parse_toml(path)
-            default.update(custom)
-
+        key = key.upper()
+        if key in self._pelicanconf.__dir__():
+            return getattr(self._pelicanconf, key)
+        elif key in self._default.__dir__():
+            return getattr(self._default, key)
         return default
 
-    def parse_toml(self, path=None):
-        '''从 filepath 中解析 toml 配置文件
+    def update(self, key, value):
+        '''更新配置
         '''
-        config = {}
-        if path is None or not os.path.exists(path):
-            path = os.path.join(os.path.dirname(__file__), './config/pelican_manager.toml')
+        return self.set(key, value)
 
-        config.update(toml.load(path))
-        return config
+    def save(self):
+        ''' 保存环境变量到 self._path 路径'''
+        update = []
+        lines = []
+        # if os.path.exists(self._path):
+        with open(self._path, 'r') as fp:
+            lines = fp.read().split('\n')
+
+        for key, value in list(self.__dict__.items()):
+            # 排除掉开头为下划线的内部变量
+            if key[0] != '_':
+                key = key.upper()
+                def filter_func(line):
+                    match = re.match(r'^{}(\s.*)=(.*)'.format(key), line)
+                    if match:
+                        return True
+                result = list(filter(filter_func, lines))
+                if isinstance(value, str):
+                    value = '\'{}\''.format(value)
+                attribute = '{key} = {value}'.format(key=key, value=value)
+                if result:
+                    index = lines.index(result[0])
+                    lines[index] = attribute
+                else:
+                    if key == 'SERVER_PORT':
+                        lines.append('# Server running port.')
+                    lines.append(attribute)
+                # 删除更新过的 key
+                try:
+                    del self.__dict__[key.lower()]
+                except KeyError as e:
+                    del self.__dict__[key.upper()]
+                else:
+                    print("未知 key:{}".format(key))
+
+        text = '{}'.format(os.linesep).join(lines)
+        with open(self._path, 'w+') as fp:
+            fp.write(text)
+
+    def make_config(self, path, name):
+        '''make config'''
+        pelicanconf = import_module(name, path)
+        return pelicanconf
+
+    def __getattr__(self, key):
+        return self.get(key)
+
+    def __setattr__(self, key, value):
+        self.set(key, value)
+
+    def __getitem__(self, key):
+        return self.get(key)
