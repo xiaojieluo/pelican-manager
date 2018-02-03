b7eab85dd841ceca23f278d78b64e93db594a511
xiaojieluo <xiaojieluoff@gmail.com>
2018-02-03 14:04:18 +0800

    add -c|--config command

--- pelican_manager/__main__.py
+++ pelican_manager/__main__.py
@@ -1,28 +1,27 @@
 import argparse
 import sys, os
 from pelican_manager import make_app
-import toml
 from pelican_manager.config import Config
-# from .utils import make_config
 
 
 def parse_args(args):
     ''' parse args'''
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-p', '--port', nargs='?', type=int)
+    parser.add_argument('-c', '--config', nargs='?', type=str)
 
-    args = parser.parse_args()
+    args = parser.parse_args(args)
     return args
 
 
 def main():
     '''enter'''
-    # config = make_config()
-    config = Config()
     args = parse_args(sys.argv[1:])
-    app = make_app()
+    # print(args)
+    app = make_app(args.config)
+    config = Config()
     app.run(
-        port=args.port or config['server']['port'],
+        port=args.port or config.server_port,
         )
 
 if __name__ == '__main__':
